df80dcc660d851d0eef26302396b4cd7b810e06c
Dmitry Romashov <framebassman@gmail.com>
2024-06-17 21:53:37 +0200

    Feature/new boilerplate (#1)
    * Remove all files
    
    * Add boilerplate
    
    * Update template
    
    * Add a basic component
    
    * Fix font size
    
    * Fix font

---
+++ app/core/init.ts
@@ -0,0 +1,14 @@
+/**
+ * Svelte Init
+ * =====================
+ * Create svelte app
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+import app from "@app/html/hello.svelte";
+import "../styles/hello.css";
+
+export default app;
---
+++ app/html/hello.svelte
@@ -0,0 +1,15 @@
+<svelte:options tag="svelte-hello-boilerplate" />
+
+<script lang="ts">
+</script>
+
+<div class="title">
+	<div class="text">
+		<p>Привет</p>
+		<p>Мы — Коленка Инкорпорейтед</p>
+	</div>
+</div>
+
+<style lang="css">
+	@import "./bundle.css";
+</style>
