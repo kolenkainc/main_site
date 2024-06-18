3b5d53e8ce6c5006ce6cf9277e7e57ef894244a2
Dmitry Romashov <framebassman@gmail.com>
2024-06-18 11:28:33 +0200

    Refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,37 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Kolenka Inc</title>
+  <meta name="description" content="Kolenka Inc" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+  <div class="title">
+    <div class="text">
+      <p>Привет</p>
+      <p>Мы — Коленка Инкорпорейтед</p>
+    </div>
+  </div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  @font-face {
+    font-family: Academy;
+    src: url("Academy.ttf");
+  }
 
-	h1 {
-		width: 100%;
-	}
+  .title {
+    height: 50vh;
+    font-family: Academy;
+  }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  .text {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 7.4vw;
+  }
 </style>
